6db2b4e02305a2968d09717abb83fa5f9074e1db
root <root@localhost.localdomain>
2018-11-26 04:07:44 -0600

    4

---
+++ bandit/level4.py
@@ -0,0 +1,16 @@
+#!/bin/python2
+
+import pwn
+
+host = "bandit.labs.overthewire.org"
+user = "bandit3"
+passwd = "UmHadQclWmgdLOKQ3YNgjWxGoRMb5luK"
+port = 2220
+
+shell = pwn.ssh(user, host, port, passwd)
+new_passwd = shell.run('cd ./inhere && cat "$(ls -a | egrep .[a-z]+)"').recvall().strip("\n")
+shell.close()
+
+shell = pwn.ssh("bandit4", host, port, new_passwd)
+
+print shell.ls()
